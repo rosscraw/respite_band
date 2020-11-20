5f1d702fed14ac1f2b3c9b926d6a8ab4aebbaf24
Ross Crawford <ross.crawford.2013@uni.strath.ac.uk>
2020-11-20 01:06:12 +0000

    Introduced a Settings screen. Changed the way band member images were loaded to perform faster.

--- lib/views/members/members_screen.dart
+++ lib/views/members/members_screen.dart
@@ -3,6 +3,11 @@ import 'package:checklist/views/member_details/member_details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+
+/// Screen that provides a list of all the [BandMember]s.
+///
+/// Shows the member's name, their role and a picture.
+/// Can access further information for each member by selecting the information button.
 class MembersScreen extends StatefulWidget {
   MembersScreen({Key key, this.title}) : super(key: key);
 
@@ -14,35 +19,8 @@ class MembersScreen extends StatefulWidget {
 
 class _MembersScreenState extends State<MembersScreen> {
 
-
-//  List<BandMember> bandMembers = [
-//    Constant.sam,
-//    Constant.andrew,
-//    Constant.euan,
-//    Constant.ross,
-//    Constant.reiss
-//  ];
-
-
-
-
-//  List<BandMember> bandMembers = [
-//    BandMember(name: 'Samuel Nicholson', plays: 'Vocals', image: Image.asset('images/members/sam/sam.jpg')),
-//    BandMember(name: 'Andrew Vaughan', plays: 'Guitar', image: Image.asset('images/members/andrew/andrew.jpg')),
-//    BandMember(name: 'Euan Macqueen', plays: 'Guitar', image: Image.asset('images/members/euan/euan.jpg')),
-//    BandMember(name: 'Ross Crawford', plays: 'Bass', image: Image.asset('images/members/ross/ross.jpg')),
-//    BandMember(name: 'Reiss McLeod', plays: 'Drums', image: Image.asset('images/members/reiss/reiss.jpg')),
-//  ];
-
-
-
   @override
   Widget build(BuildContext context) {
-//    Constant.ross.addInstrument(Constant.modelt);
-//    Constant.ross.addInstrument(Constant.aerodyne);
-//    Constant.andrew.addInstrument(Constant.charvel);
-//    Constant.andrew.addInstrument(Constant.prs);
-//    Constant.andrew.addInstrument(Constant.pawnshop);
 
   final bandMembers = Provider.of<List<BandMember>>(context);
 
@@ -57,36 +35,66 @@ class _MembersScreenState extends State<MembersScreen> {
                 shrinkWrap: true,
                 itemCount: bandMembers.length,
                 itemBuilder: (context, index) {
-                  return Card(
-                    color: Colors.grey[900],
-                    child: ListTile(
-                      //TODO update to circle icon of member
-                      leading: CircleAvatar(
-                        backgroundImage: bandMembers[index].image.image,
-                        //child: bandMembers[index].image
-                      ),
-                      //TODO Style
-                      title: Text(bandMembers[index].name, style: GoogleFonts.robotoSlab(color: const Color.fromARGB(205, 212, 175, 55),)),
-                      //TODO Style
-                      subtitle: Text(bandMembers[index].plays, style: GoogleFonts.robotoSlab(color: const Color.fromARGB(205, 212, 175, 55),)),
-                      //TODO take to separate page
-                      trailing: FlatButton.icon(
-                        icon: Icon(Icons.info_outline, color: const Color.fromARGB(205, 212, 175, 55),
-                        ),
-                        label: Text('info', style: GoogleFonts.robotoSlab(color: const Color.fromARGB(205, 212, 175, 55),)),
-                        //TODO Navigate to band member details page
-                        onPressed: () {
-                          navigateToMemberDetails(bandMembers[index]);
-                        },
-                      ),
-
-                    )
-                  );
+                  return memberCard(index, bandMembers);
+//                  return Card(
+//                    color: Colors.grey[900],
+//                    child: ListTile(
+//                      //TODO update to circle icon of member
+//                      leading: CircleAvatar(
+//                        backgroundImage: bandMembers[index].image.image,
+//                        //child: bandMembers[index].image
+//                      ),
+//                      //TODO Style
+//                      title: Text(bandMembers[index].name, style: GoogleFonts.robotoSlab(color: const Color.fromARGB(205, 212, 175, 55),)),
+//                      //TODO Style
+//                      subtitle: Text(bandMembers[index].plays, style: GoogleFonts.robotoSlab(color: const Color.fromARGB(205, 212, 175, 55),)),
+//                      //TODO take to separate page
+//                      trailing: FlatButton.icon(
+//                        icon: Icon(Icons.info_outline, color: const Color.fromARGB(205, 212, 175, 55),
+//                        ),
+//                        label: Text('info', style: GoogleFonts.robotoSlab(color: const Color.fromARGB(205, 212, 175, 55),)),
+//                        //TODO Navigate to band member details page
+//                        onPressed: () {
+//                          navigateToMemberDetails(bandMembers[index]);
+//                        },
+//                      ),
+//
+//                    )
+//                  );
                 }
             )
           ),
         ),
       ),
+    );
+  }
+
+  /// This produces a [Card] which displays information about a [BandMember].
+  /// Can access [MemberDetailsScreen] for specific member by selecting the trailing [FlatButton].
+  Card memberCard(int index, List<BandMember> bandMembers) {
+    return Card(
+        color: Colors.grey[900],
+        child: ListTile(
+          //TODO update to circle icon of member
+          leading: SizedBox(
+            child: bandMembers[index].image
+          ),
+          //TODO Style
+          title: Text(bandMembers[index].name, style: GoogleFonts.robotoSlab(color: const Color.fromARGB(205, 212, 175, 55),)),
+          //TODO Style
+          subtitle: Text(bandMembers[index].plays, style: GoogleFonts.robotoSlab(color: const Color.fromARGB(205, 212, 175, 55),)),
+          //TODO take to separate page
+          trailing: FlatButton.icon(
+            icon: Icon(Icons.info_outline, color: const Color.fromARGB(205, 212, 175, 55),
+            ),
+            label: Text('info', style: GoogleFonts.robotoSlab(color: const Color.fromARGB(205, 212, 175, 55),)),
+            //TODO Navigate to band member details page
+            onPressed: () {
+              navigateToMemberDetails(bandMembers[index]);
+            },
+          ),
+
+        )
     );
   }
 
